4899ffa24c5316e4bbe1136c01e7ded06f49fd1e
Carlos Mendes Aderaldo <cmendesce@gmail.com>
2016-09-08 21:29:13 -0300

    Add token class

---
+++ app/token.js
@@ -0,0 +1,53 @@
+'use strict'
+
+var exports = module.exports = {}
+
+const tokenType = {
+  OPEN: 'OPEN',
+  CLOSE: 'CLOSE',
+  NOT: 'NOT',
+  AND: 'AND',
+  OR: 'OR',
+  IMPLIES: 'IMPLIES',
+  PREMISE: 'PREMISE'
+}
+
+exports.Type = tokenType
+
+exports.create = function(part) {
+  let type;
+  let value = part;
+  switch (part) {
+    case '(':
+      type = tokenType.OPEN
+      break;
+    case ')':
+      type = tokenType.CLOSE
+      break;
+    case '^':
+      type = tokenType.AND
+      break;
+    case 'V':
+      type = tokenType.OR
+      break;
+    case '~':
+      type = tokenType.NOT
+      break;
+    case '->':
+      value = '\u2192'
+      type = tokenType.IMPLIES
+      break;
+    default:
+      if (/([A-Z])/g.test(part)) {
+        type = tokenType.PREMISE
+      }
+    }
+    if (!type) {
+      return undefined
+    } else {
+      return {
+        type: type,
+        value: value
+      }
+    }
+}
---
+++ spec/token-spec.js
@@ -0,0 +1,41 @@
+"use strict"
+
+describe('should create token from char', function() {
+  const token = require('../app/token.js')
+  it('and', function() {
+    const t = token.create('^')
+    expect(t.type).toBe(token.Type.AND)
+  })
+  it('or', function() {
+    const t = token.create('V')
+    expect(t.type).toBe(token.Type.OR)
+  })
+  it('not', function() {
+    const t = token.create('~')
+    expect(t.type).toBe(token.Type.NOT)
+  })
+  it('open', function() {
+    const t = token.create('(')
+    expect(t.type).toBe(token.Type.OPEN)
+  })
+  it('clone', function() {
+    const t = token.create(')')
+    expect(t.type).toBe(token.Type.CLOSE)
+  })
+  it('implies', function() {
+    const t = token.create('->')
+    expect(t.type).toBe(token.Type.IMPLIES)
+  })
+  it('premise', function() {
+    const t = token.create('A')
+    expect(t.type).toBe(token.Type.PREMISE)
+  })
+  it('double premise', function() {
+    const t = token.create('AB')
+    expect(t.type).toBe(token.Type.PREMISE)
+  })
+  it('invalid token', function() {
+    const t = token.create('&')
+    expect(t).toBeUndefined()
+  })
+})
