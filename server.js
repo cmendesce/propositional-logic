b276f473fc1b3783ee26ac448a781f695cd023ad
Carlos Mendes Aderaldo <cmendesce@gmail.com>
2016-09-12 10:03:09 -0300

    Add first ui stable version

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const evaluator = require('./app/compiler.js')
+const compiler = require('./app/compiler.js')
     , express = require('express')
     , logger = require('morgan')
     , bodyParser = require('body-parser')
 
 const app = module.exports.app = express()
 app.use(logger('dev'))
-app.use(express.static(__dirname + '/public/static'))
+app.use(express.static(__dirname + '/public/js'))
 app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 5000));
 
@@ -21,7 +21,7 @@ app.get('/about', function (req, res) {
 app.post('/evaluate', function(request, response) {
   const expression = request.body.expression
   console.log('evaluating the expression --->', expression)
-  const tree = evaluator.asTree(expression)
+  const tree = compiler.syntaxTree(expression)
 
   response.status(200).set('Content-Type', 'application/json').send(tree)
 })
