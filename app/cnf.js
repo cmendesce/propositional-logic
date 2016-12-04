beb56b1c2677828a9e014af44bba53a22c1ac393
Carlos Mendes Aderaldo <cmendesce@gmail.com>
2016-12-04 15:37:47 -0300

    Remove implies

---
+++ app/cnf.js
@@ -0,0 +1,113 @@
+'use strict'
+
+
+const token = require('../app/token.js')
+const tokenType = require('../app/tokenType').Type
+const ast = require('../app/ast.js')
+const astType = require('../app/astType.js').AstType
+
+const convert = (exp) => {
+	const root = ast.get(exp)
+
+	/*
+	passo1: elimine o conectivo → usando: 
+		 α → β ≡ (¬α ∨ β)
+		¬(α → β) ≡ (α ∧ ¬β)
+	*/
+	if (root.type === astType.BINARY) {
+		const left = root.children[0]
+		const right = root.children[1]
+
+		if (root.token.type === tokenType.IMPLIES) {
+				return or(not(left), right)
+		}
+	} else {
+		if (!!root.children && root.children.length > 0) {
+			
+			if (root.children[0].token.type === tokenType.IMPLIES) { // ~(A x B)	
+				const left = root.children[0].children[0]
+				const right = root.children[0].children[1]
+
+				return and(left, not(right));
+			}
+		}
+		return root;
+	}
+};
+
+const not = (exp) => {
+	return {
+		type: astType.UNARY,
+		token: {type: tokenType.NOT, value: '~'},
+		children: [exp]
+	}
+}
+
+const or = (left, right) => {
+	return {
+		type: astType.BINARY,
+    token: {type: tokenType.OR, value: 'v'},
+    children: [left, right]
+	};
+}
+
+const and = (left, right) => {
+	return {
+		type: astType.BINARY,
+    token: {type: tokenType.AND, value: '^'},
+    children: [left, right]
+	};
+}
+
+module.exports = {convert:convert};
+
+//console.log(JSON.stringify(convert('(A -> B)')))
+
+
+
+/*
+passo1: elimine o conectivo → usando: 
+α → β ≡ (¬α ∨ β)
+¬(α → β) ≡ (α ∧ ¬β)
+
+passo 2: mova a negação (¬) para o interior da fórmula, usando as seguintes regras: ¬¬α ≡ α
+¬(α ∧ β) ≡ (¬α ∨ ¬β)
+¬(α ∨ β) ≡ (¬α ∧ ¬β)
+
+passo3: mova as conjunções para o exterior da fórmula usando:
+α ∨ (β ∧ γ) ≡ (α ∨ β) ∧ (α ∨ γ)
+(α ∧ β) ∨ γ ≡ (α ∨ γ) ∧ (β ∨ γ)
+
+
+Exemplo:
+(A∨B)→C⇒ passo1 ⇒¬(A∨B)∨C⇒ passo2 ⇒ (¬A∧¬B)∨C ⇒ passo3 ⇒(¬A∨C)∧(¬B∨C)FNC
+
+
+public static Formula fixNegations (Formula f) { //Move negations in (until we have none)
+		if (f instanceof NotFormula) { //NOT
+			if (((NotFormula) f).p instanceof NotFormula) { //NOT NOT
+				return fixNegations(((NotFormula)((NotFormula) f).p).p);
+			}
+			else if (((NotFormula) f).p instanceof AndFormula) { //NOT AND
+				return new OrFormula(fixNegations(new NotFormula(((AndFormula)((NotFormula) f).p).p)), fixNegations(new NotFormula(((AndFormula)((NotFormula) f).p).q)));
+			}
+			else if (((NotFormula) f).p instanceof OrFormula) { //NOT OR
+				return new AndFormula(fixNegations(new NotFormula(((OrFormula)((NotFormula) f).p).p)), fixNegations(new NotFormula(((OrFormula)((NotFormula) f).p).q)));
+			}
+			else if (((NotFormula)f).p instanceof BoolFormula) { //NOT CONST
+				return new BoolFormula(!((BoolFormula)((NotFormula)f).p).b);
+			}
+			else { //NOT VAR
+				return fixNegations(((NotFormula)f).p);
+			}
+		}
+		else if (f instanceof AndFormula) {
+			return new AndFormula (fixNegations(((AndFormula)f).p), fixNegations(((AndFormula)f).q));
+		}
+		else if (f instanceof OrFormula) {
+			return new OrFormula (fixNegations(((OrFormula)f).p), fixNegations(((OrFormula)f).q));
+		}
+		else { // VAR or CONST
+			return f;
+		}
+	}*/
---
+++ spec/cnf-spec.js
@@ -0,0 +1,64 @@
+/*
+passo1: elimine o conectivo → usando: 
+α → β ≡ (¬α ∨ β)
+¬(α → β) ≡ (α ∧ ¬β)
+
+passo 2: mova a negação (¬) para o interior da fórmula, usando as seguintes regras: 
+¬¬α ≡ α
+¬(α ∧ β) ≡ (¬α ∨ ¬β)
+¬(α ∨ β) ≡ (¬α ∧ ¬β)
+
+passo3: mova as conjunções para o exterior da fórmula usando:
+α ∨ (β ∧ γ) ≡ (α ∨ β) ∧ (α ∨ γ)
+(α ∧ β) ∨ γ ≡ (α ∨ γ) ∧ (β ∨ γ)
+*/
+
+'use strict'  
+
+describe('CNF: ', () => {
+  const token = require('../app/token')
+  const tokenType = require('../app/tokenType').Type
+  const astType = require('../app/astType').AstType
+  const cnf = require('../app/cnf')
+  const ast = require('../app/ast')
+
+  
+
+  describe('Basic', () => {
+    
+    it('P should be P', () => {
+      const expected = {
+        type: astType.PROP,
+        token: {type: tokenType.PREMISE, value: 'P'}
+      }
+      expect(cnf.convert('(P)')).toEqual(expected)
+    })
+
+    it('(~P) should be (~P)', () => {
+      const expected = {
+        type: astType.UNARY,
+        token: {type: tokenType.NOT, value: '~'},
+        children: [
+          { type: astType.PROP,
+            token: {type: tokenType.PREMISE, value: 'P'}
+          }
+        ]
+      }
+      expect(cnf.convert('(~P)')).toEqual(expected)
+    })
+  })
+
+  describe('Remove implies', () => {
+    it('(A -> B) should be ((~A) v B)', () => {
+      expect(cnf.convert('(A -> B)')).toEqual(ast.get('(~A v B)'))
+    })
+
+    it('((A v C ^ D) -> (B ^ A)) should be (~(A v C ^ D) v (B ^ A))', () => {
+      expect(cnf.convert('(A -> B)')).toEqual(ast.get('(~A v B)'))
+    })
+
+    it('(~(A -> B)) should be (A ^ ~B)', () => {
+      expect(cnf.convert('~(A -> B)')).toEqual(ast.get('(A ^ ~B)'))
+    })
+  })
+})
